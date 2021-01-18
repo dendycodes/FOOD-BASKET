9b671a228865082e074106fe175648a4228bd39a
Lyotin99 <71409850+Lyotin99@users.noreply.github.com>
2021-01-19 01:31:15 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app=express();
-const {getComments,postOneComment, deleteComment}=require('./handlers/comments')
-const {getAllOrders,postOrder, getOrder, commentOnOrder, deleteOrder} = require("./handlers/orders");
-const {signup,login,uploadImage}=require('./handlers/users')
+const {getComments,postOneComment, deleteComment, editComment}=require('./handlers/comments')
+const {getAllOrders,postOrder, getOrder, commentOnOrder, deleteOrder, editOrder} = require("./handlers/orders");
+const {signup,login,uploadImage, deleteUser}=require('./handlers/users')
 const FBAuth=require('./util/fbAuth');
 app.use(express.json());
 
@@ -18,10 +18,13 @@ app.post('/comments',FBAuth,postOneComment)
 app.post('/orders',FBAuth,postOrder)
 app.post('/user/image',FBAuth,uploadImage);
 
+app.put('/order/:orderId',FBAuth,editOrder);
+app.put('/comment/:commentId',FBAuth,editComment);
 
 app.delete('/order/:orderId',FBAuth,deleteOrder);
 app.delete('/comment/:commentId',FBAuth,deleteComment);
-
+app.delete('/user/:userId',FBAuth,deleteUser);
 
 exports.api=functions.region('europe-west1').https.onRequest(app);
 
+ 
