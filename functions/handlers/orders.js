eb179bbb8083e28d7b59a8087829ad99dd82559f
Lyotin99 <71409850+Lyotin99@users.noreply.github.com>
2021-01-21 22:59:31 +0200

    Update orders.js

--- functions/handlers/orders.js
+++ functions/handlers/orders.js
@@ -1,190 +1,196 @@
-const{db}=require('../util/admin')
+const { db } = require("../util/admin");
 
-exports.getAllOrders=(req,res)=>{
-    db.collection('orders')
+exports.getAllOrders = (req, res) => {
+  db.collection("orders")
     .get()
-    .then((data)=>{
-      let order=[];
-      data.forEach((doc)=>{
+    .then((data) => {
+      let order = [];
+      data.forEach((doc) => {
         order.push(doc.data());
       });
       return res.json(order);
     })
-    .catch((err)=>{
+    .catch((err) => {
       console.error(err);
       res.status(500).json({
-        error:err.code
+        error: err.code,
       });
-    })
-  }
-
-  exports.postOrder=(req,res)=>{
-    if(req.body.orderName.trim()===''){
-      return res.status(400).json({orderName:'Order must not be empty.'})
-    }
-    if(req.body.hour>23 || req.body.hours<0){
-      return res.status(400).json({hour:'Must be a valid hour.'})
-    }
-
-    if(req.body.minutes>59 || req.body.minutes<0){
-      return res.status(400).json({minutes:'Must be valid minutes'})
-    }
-  const newOrder={
-    orderName:req.body.orderName,
-    hour:req.body.hour,
-    minutes:req.body.minutes,
-    username:req.user.username,
-    createdAt:new Date()
-  }
-  
-  db.collection('orders').add(newOrder).then((doc)=>{
-    res.json({message:`document ${doc.id} created successfully`})
-  })
-  .catch((err)=>{
-    res.status(500).json({error:'something went wrong'})
-    console.log(err);
-  })
-    }
-
-
-
-    exports.getOrder=(req,res)=>{
-      let orderData={};
-      db.doc(`/orders/${req.params.orderId}`)
-      .get()
-      .then((doc) => {
-        if(!doc.exists){
-          return res.status(404).json({error:'Order not found'})
-        }
-        
-        orderData=doc.data();
-        orderData.orderId=doc.id;
-        return db.collection('comments').orderBy('createdAt','desc').where('orderId','==',req.params.orderId)
-        .get();
-      })
-        .then(data=>{
-          orderData.comments=[];
-          data.forEach(doc=>{
-            orderData.comments.push(doc.data());
-          })
-          return res.json({orderData} )
-        })
-        .catch(err=>{
-          console.error(err);
-          res.status(500).json({errоr:err.code})
-        })
-      
-    }
-
-    exports.commentOnOrder=(req,res)=>{
-if(req.body.comment.trim()===''){
-  return res.status(400).json({error:'Must not be empty'})
-}
-const newComment={
-  comment:req.body.comment,
-  price:req.body.price,
-  createdAt:new Date(),
-  orderId:req.params.orderId,
-  username:req.user.username,
-  userImage:req.user.imageUrl
+    });
 };
-db.doc(`/orders/${req.params.orderId}`)
-.get()
-.then(doc=>{
-  if(!doc.exists){
-    return res.status(404).json({error:'Order not found'})
+
+exports.postOrder = (req, res) => {
+  if (req.body.orderName.trim() === "") {
+    return res.status(400).json({ orderName: "Order must not be empty." });
   }
-let currentDate=newComment.createdAt;
-
-let requestedHour=parseInt(doc.data().hour);
-let requestedMinutes=parseInt(doc.data().minutes);
-
-if(currentDate.getHours()>requestedHour){
-  return res.json({error:'The order is expired!'})
-}
-else{
-  if(currentDate.getHours()===requestedHour && currentDate.getMinutes()>requestedMinutes){
-    return res.json({error:'The order is expired!'})
-  }else{
-    return res.status(201).json(newComment);
+  if (req.body.hour > 23 || req.body.hours < 0) {
+    return res.status(400).json({ hour: "Must be a valid hour." });
   }
-}
 
-  
-})
-.then(()=>{
-  res.json(newComment)
-})
-.catch(err=>{
-  console.error(err)
-  res.status(500).json({error:'Something went wrong'})
-})
-    }
+  if (req.body.minutes > 59 || req.body.minutes < 0) {
+    return res.status(400).json({ minutes: "Must be valid minutes" });
+  }
+  const newOrder = {
+    orderName: req.body.orderName,
+    hour: req.body.hour,
+    username: req.user.username,
+    createdAt: new Date(),
+    minutes: req.body.minutes,
+  };
 
+  db.collection("orders")
+    .add(newOrder)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.log(err);
+    });
+};
 
-    exports.deleteOrder=(req,res)=>{
-      const document=db.doc(`/orders/${req.params.orderId}`);
-      document.get()
-      .then(doc=>{
-        if(!doc.exists){
-          return res.status(404).json({error:'Order not found'})
-        }
-        if(doc.data().username!==req.user.username){
-          if(req.user.username==='admin'){
-            return document.delete()
-          }else return res.status(403).json({error:'Unauthorized'})
-        }
-        else{
-          return document.delete()
-        }
-      })
-      .then(()=>{
-        res.json({message:'Order deleted successfully'})
-      })
-      .catch(err=>{
-        console.error(err)
-        return res.status(500).json({error:err.code})
-      })
-    }
-
-
-    exports.editOrder=(req,res)=>{
-      const document=db.doc(`/orders/${req.params.orderId}`);
-      document.get()
-      .then(doc=>{
-        if(!doc.exists){
-          return res.status(404).json({error:'Order not found'})
-        }
-        if(req.body.orderName.trim()===''){
-          return res.status(400).json({orderName:'Order must not be empty.'})
-        }   
-        if(req.body.hour>23 || req.body.hour<0){
-          return res.status(400).json({hour:'Must be a valid hour.'})
-        }
-    
-        if(req.body.minutes>59 || req.body.minutes<0){
-          return res.status(400).json({minutes:'Must be valid minutes'})
-        }
-      const newOrder=req.body;
-      
-       if(doc.data().username!==req.user.username){
-         if(req.user.username==='admin'){
-          document.update(newOrder);
-         return res.json({message:`document ${req.params.orderId} edited successfully`});
-         }
-        return res.status(403).json({error:'Unauthorized'})
-        }
-      document.update(newOrder).then((el)=>{
-        res.json({message:`document ${req.params.orderId} edited successfully`})
-      })
-      .catch((err)=>{
-        res.status(500).json({error:'something went wrong'})
-        console.log(err);
-      })
-
- 
-
-
+exports.getOrder = (req, res) => {
+  let orderData = {};
+  db.doc(`/orders/${req.params.orderId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Order not found" });
       }
-    
-      )}
+
+      orderData = doc.data();
+      orderData.orderId = doc.id;
+      return db
+        .collection("comments")
+        .orderBy("createdAt", "desc")
+        .where("orderId", "==", req.params.orderId)
+        .get();
+    })
+    .then((data) => {
+      orderData.comments = [];
+      data.forEach((doc) => {
+        orderData.comments.push(doc.data());
+      });
+      return res.json({ orderData });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ errоr: err.code });
+    });
+};
+
+exports.commentOnOrder = (req, res) => {
+  if (req.body.comment.trim() === "") {
+    return res.status(400).json({ error: "Must not be empty" });
+  }
+  if (req.body.comment.trim() === "") {
+    return res.status(400).json({ Comment: "Comment must not be empty." });
+  }
+  if (req.body.price === 0) {
+    return res.status(400).json({ Comment: "Comment must have price." });
+  }
+  const newComment = {
+    comment: req.body.comment,
+    price: req.body.price,
+    createdAt: new Date(),
+    orderId: req.params.orderId,
+    username: req.user.username,
+    userImage: req.user.imageUrl,
+  };
+  db.doc(`/orders/${req.params.orderId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Order not found" });
+      }
+      let currentDate = newComment.createdAt;
+
+      let requestedHour = parseInt(doc.data().hour);
+      let requestedMinutes = parseInt(doc.data().minutes);
+      if (currentDate.getHours() > requestedHour) {
+        return res.json({ error: "The order is expired!" });
+      } else {
+        if (
+          currentDate.getHours() === requestedHour &&
+          currentDate.getMinutes() > requestedMinutes
+        ) {
+          return res.json({ error: "The order is expired!" });
+        } else {
+          db.collection("comments").add(newComment);
+          return res.status(201);
+        }
+      }
+    })
+    .then(() => {
+      res.json({ message: "Document created successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+exports.deleteOrder = (req, res) => {
+  const document = db.doc(`/orders/${req.params.orderId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Order not found" });
+      }
+      if (doc.data().username !== req.user.username) {
+        if (req.user.username === "admin") {
+          return document.delete();
+        } else return res.status(403).json({ error: "Unauthorized" });
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.json({ message: "Order deleted successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+exports.editOrder = (req, res) => {
+  const document = db.doc(`/orders/${req.params.orderId}`);
+  document.get().then((doc) => {
+    if (!doc.exists) {
+      return res.status(404).json({ error: "Order not found" });
+    }
+    if (req.body.orderName.trim() === "") {
+      return res.status(400).json({ orderName: "Order must not be empty." });
+    }
+    if (req.body.hour > 23 || req.body.hour < 0) {
+      return res.status(400).json({ hour: "Must be a valid hour." });
+    }
+
+    if (req.body.minutes > 59 || req.body.minutes < 0) {
+      return res.status(400).json({ minutes: "Must be valid minutes" });
+    }
+    const newOrder = req.body;
+
+    if (doc.data().username !== req.user.username) {
+      if (req.user.username === "admin") {
+        document.update(newOrder);
+        return res.json({
+          message: `document ${req.params.orderId} edited successfully`,
+        });
+      }
+      return res.status(403).json({ error: "Unauthorized" });
+    }
+    document
+      .update(newOrder)
+      .then((el) => {
+        res.json({
+          message: `document ${req.params.orderId} edited successfully`,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "something went wrong" });
+        console.log(err);
+      });
+  });
+};
