715ec8c7fd864d13d1b765e9a0030be90058310f
Lyotin99 <71409850+Lyotin99@users.noreply.github.com>
2021-01-21 23:00:11 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,110 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const app=express();
-const {getComments,postOneComment, deleteComment, editComment}=require('./handlers/comments')
-const {getAllOrders,postOrder, getOrder, commentOnOrder, deleteOrder, editOrder} = require("./handlers/orders");
-const {signup,login,uploadImage, deleteUser}=require('./handlers/users')
-const FBAuth=require('./util/fbAuth');
+const app = express();
+const {
+  getComments,
+  postOneComment,
+  deleteComment,
+  editComment,
+} = require("./handlers/comments");
+const {
+  getAllOrders,
+  postOrder,
+  getOrder,
+  commentOnOrder,
+  deleteOrder,
+  editOrder,
+} = require("./handlers/orders");
+const { signup, login, uploadImage, deleteUser } = require("./handlers/users");
+const FBAuth = require("./util/fbAuth");
+const { db } = require("./util/admin");
 app.use(express.json());
 
-app.get('/orders',getAllOrders)
-app.get('/orders/:orderId',getOrder)
-app.get('/comments',getComments)
+app.get("/orders", getAllOrders);
+app.get("/orders/:orderId", getOrder);
+app.get("/comments", getComments);
 
-app.post('/signup',signup)
-app.post('/login',login)
-app.post('/orders/:orderId/comment',FBAuth,commentOnOrder)
-app.post('/comments',FBAuth,postOneComment)
-app.post('/orders',FBAuth,postOrder)
-app.post('/user/image',FBAuth,uploadImage);
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/orders/:orderId/comment", FBAuth, commentOnOrder);
+app.post("/comments", FBAuth, postOneComment);
+app.post("/orders", FBAuth, postOrder);
+app.post("/user/image", FBAuth, uploadImage);
 
-app.put('/order/:orderId',FBAuth,editOrder);
-app.put('/comment/:commentId',FBAuth,editComment);
+app.put("/order/:orderId", FBAuth, editOrder);
+app.put("/comment/:commentId", FBAuth, editComment);
 
-app.delete('/order/:orderId',FBAuth,deleteOrder);
-app.delete('/comment/:commentId',FBAuth,deleteComment);
-app.delete('/user/:userId',FBAuth,deleteUser);
+app.delete("/order/:orderId", FBAuth, deleteOrder);
+app.delete("/comment/:commentId", FBAuth, deleteComment);
+app.delete("/user/:userId", FBAuth, deleteUser);
 
-exports.api=functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region("europe-west1").https.onRequest(app);
 
- 
+exports.onUserImageChange = functions
+  .region("europe-west1")
+  .firestore.document("/Users/{userId}")
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      const batch = db.batch();
+      return db
+        .collection("orders")
+        .where("username", "==", change.before.data().username)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const order = db.doc(`/orders/${doc.id}`);
+            batch.update(order, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+exports.onOrderDelete = functions
+  .region("europe-west1")
+  .firestore.document("/orders/{orderId}")
+  .onDelete((snapshot, context) => {
+    const orderId = context.params.orderId;
+    const batch = db.batch();
+    return db
+      .collection("comments")
+      .where("orderId", "==", orderId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+exports.onUserDelete = functions
+  .region("europe-west1")
+  .firestore.document("/Users/{userId}")
+  .onDelete((snapshot, context) => {
+    const userId = context.params.userId;
+    const batch = db.batch();
+    return db
+      .collection("comments")
+      .where("username", "==", userId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection("orders").where("username", "==", userId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/orders/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
